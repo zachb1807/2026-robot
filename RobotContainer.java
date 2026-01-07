54d4ee077a54234b3fa95590a778f255d0ceae35
Zach B <56662213+zachb1807@users.noreply.github.com>
2026-01-07 16:12:55 -0600

    Add readme, add get position method, remove gripper motor port

--- RobotContainer.java
+++ RobotContainer.java
@@ -1,4 +1,9 @@
 public static final int ARM_MOTOR_PORT = 5;
-public static final int GRIPPER_MOTOR_PORT = 6;
 public static final int ARM_ENCODER_PORT_A = 7;
+
+public static int getPosition() {
+    // Placeholder for actual implementation
+    return 50;
+}
+
 
