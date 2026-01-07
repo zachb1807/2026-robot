40c9084cd0eee560b7481ec1f615cfc6a10827ea
Zach B <56662213+zachb1807@users.noreply.github.com>
2026-01-07 16:14:12 -0600

    remove get position comment

--- RobotContainer.java
+++ RobotContainer.java
@@ -2,7 +2,6 @@ public static final int ARM_MOTOR_PORT = 5;
 public static final int ARM_ENCODER_PORT_A = 7;
 
 public static int getPosition() {
-    // Placeholder for actual implementation
     return 50;
 }
 
